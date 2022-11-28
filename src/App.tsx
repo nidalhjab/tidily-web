d268366db0cb3d9c08d812ada13e935468479764
nidalhjab <nidal_hjab@hotmail.com>
2022-11-29 01:58:45 +0200

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ import { NavBar } from "./common/components/NavBar/NavBar";
 
 
 const link = from([
-  new HttpLink({ uri: "https://capable-fudge-16fb85.netlify.app/graphql" })
+  new HttpLink({ uri: "https://capable-fudge-16fb85.netlify.app" })
 ])
 
 const authLink = setContext((_, { headers }) => {
